b9f5d4f533b89f3953f718f1c8e6a1b2563affe8
Mahdei <khmahdee@gmail.com>
2020-09-14 12:57:01 +0430

    edit 24.6.99

--- GetComments.php
+++ GetComments.php
@@ -5,7 +5,15 @@
     if(isset($_POST['user_id'])){
         $comments = array();
         $user_id = $_POST['user_id'];
-    
+        
+        $sql = "SELECT username FROM user WHERE id='$user_id'";
+        $result = $db->query($sql);
+        $result=$result->fetch_assoc(); 
+
+
+        // foreach(){
+
+        // }
 
         $sql=$db->query("select id from comment WHERE user_id='$user_id'");
         $User_id = $sql->fetch_assoc();
@@ -16,7 +24,10 @@
         
         while ($row=$sql->fetch_assoc())
         {
-                $comments[]=$row;
+            
+            $userComment =$row['user_comment'];
+            
+            $comments[]=$row;
         }
 
     
